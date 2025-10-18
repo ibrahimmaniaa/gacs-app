02160236ff1b45f2fc18a6c0bc50ad2845c29457
ibrahimmaniaa94 <ebrahimmm1994@gmail.com>
2025-10-19 00:14:19 +0200

    restructure, separation of concerns, apply MVVM archietcture

--- src/Services/MyService.cs
+++ src/Services/MyService.cs
@@ -4,7 +4,7 @@ public class MyService : IMyService
 {
   public async Task<string> DoWorkAsync(string? opt1, string? opt2)
   {
-    await Task.Delay(1000); // simulate I/O or network operation
+    await Task.Delay(3000); // simulate I/O or network operation
     return $"Done: {opt1 ?? "n/a"} + {opt2 ?? "n/a"} at {DateTime.Now:T}";
   }
 }
